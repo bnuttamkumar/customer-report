98b16e597aeaeb4cdc39e3103532d203f125a6ed
Uttam Kumar <bn.uttamkumar@gmail.com>
2021-08-30 00:37:34 +0530

    complain detail init

--- complain-details.php
+++ complain-details.php
@@ -22,3 +22,27 @@
 <?
   require('./templates/footer.php');
 ?>
+
+
+
+<?php
+// require('connection.php');
+
+//if(isset($_POST['phone_number']) && !empty($_POST['phone_number']))
+// {
+//    $phone_number = $_POST['phone_number'];
+//    // $phone_number = "7260002299";
+//     $res = mysqli_query($con,"SELECT * FROM client_master WHERE client_phone='$phone_number'");
+//     $result = mysqli_fetch_assoc($res);
+//     print_r($result);
+
+//     echo "<br>";
+//     echo "<br>";
+//     echo $result['client_name'];
+    
+
+// }else{
+//         echo "<script>alert('empty filed')</script>";
+//         echo "<script>window.location.href='fetch_client_details.php'</script>";
+// }
+?>
