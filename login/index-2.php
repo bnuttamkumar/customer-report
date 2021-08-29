6b3366b5886d524243bb808d471c933d138adce5
Uttam Kumar <bn.uttamkumar@gmail.com>
2021-08-29 22:01:39 +0530

    remove comment

--- login/index-2.php
+++ login/index-2.php
@@ -1,22 +1,21 @@
 <?php
 include('../connection.php');
 // require('connection.php');
-if(1){
-// if(isset($_POST['phone_number']) && isset($_POST['password'])){
+
+ if(isset($_POST['phone_number']) && isset($_POST['password'])){
     if(isset($_POST['phone_number']) && empty($_POST['phone_number'])){
         echo "<script>alert('empty filed')</script>";
         echo "<script>window.location.href='register.php'</script>";
     }else{
-        //echo $phone_number = mysqli_real_escape_string($con,$_POST['phone_number']);
-         $phone_number = "7260002299";
+        echo $phone_number = mysqli_real_escape_string($con,$_POST['phone_number']);
+        
     }
 
     if(isset($_POST['password']) && empty($_POST['password'])){
         echo "<script>alert('empty filed')</script>";
         echo "<script>window.location.href='register.php'</script>";
     }else{
-        // echo $password = mysqli_real_escape_string($con,$_POST['password']);
-         $password = '1';
+          echo $password = mysqli_real_escape_string($con,$_POST['password']);
     }
 
     
